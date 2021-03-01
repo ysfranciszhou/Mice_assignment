5f53862a457c4caaa3570d9898d8d7b430aa1a76
ysfranciszhou <qzhou@berklee.edu>
2021-03-01 04:53:19 -0500

    Mice

---
+++ Mice/{Zhou}mice.py
@@ -0,0 +1,9 @@
+Python 3.9.1 (v3.9.1:1e5d33e9b9, Dec  7 2020, 12:10:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> mice1 = 9
+>>> mice2 = 3
+>>> print(f"{int(mice1 / mice2)}" + "\n" + " Blind\n" + "    Mice")
+3
+ Blind
+    Mice
